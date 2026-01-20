7cf3edb0da43dfd295c4aac6ae404fedcdffb3cf
Bharat Parmar <bharatsinhhparmar39@gmail.com>
2026-01-20 21:32:05 +0530

    Initial Django ecommerce project

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,24 +1,13 @@
 from pathlib import Path
-import os
-from pathlib import Path
-import dj_database_url
-from dotenv import load_dotenv
-load_dotenv()
-
-
-
-ALLOWED_HOSTS = ['*']
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
-#SECRET_KEY = 'django-insecure-(your-secret-key)'
-#DEBUG = True
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-'DIRS': [BASE_DIR / 'templates'],
+SECRET_KEY = 'django-insecure-temp-key'
+
+DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -28,26 +17,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
-    'products',
+   'products',
     'cart',
-    'orders',
+    'accounts',
 ]
-
-
-
-
-
-
-
-
-
-
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,17 +32,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 ROOT_URLCONF = 'ecommerce.urls'
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],   # important
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -74,35 +53,45 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
-STATIC_URL = '/static/'
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-#LOGIN_URL = 'login'
-#LOGIN_REDIRECT_URL = 'profile'
-#LOGOUT_REDIRECT_URL = 'login'
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
-
-RAZORPAY_KEY_ID = "rzp_test_S66UTQJZYUqGOG"
-RAZORPAY_KEY_SECRET = "jy34ZMW3dPOx8edd7H85buQI"
-
-
-
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
